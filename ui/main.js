8506b1814cb967b3804200996ddb253e5cbb918c
muhamedshibili <shibili.k.007@gmail.com>
2016-09-28 18:53:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,21 +10,21 @@ button.onclick = function () {
     // Capture the response and store it in a variable
     request.onreadystatechange = function () {
       if (request.readystate === XMLHttpRequest.DONE) { 
-         //take some action
-         if(request.status === 200) {
+         // Take some action
+         if (request.status === 200) {
              var counter = request.responseText;
              var span = document.getElementById('count');
              span.innerHTML = counter.toString();
 
          }  
-         }
+         
 
-
-    // not done yet
+      }
+    // Not done yet
     };
     
     
-    // make the request
+    // Make the request
     request.open('GET', 'http://muhamedshibili.imad.hasura-app.io/counter',true);
     request.send(null);
 };    
