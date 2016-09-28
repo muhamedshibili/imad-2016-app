afeea0ba0723b6cdbcf54af470991940ba89119b
muhamedshibili <shibili.k.007@gmail.com>
2016-09-28 18:47:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,32 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function () {
     
-    // Make a request to the counter endpoint
-    
+    // create a request object
+    var request = new XMLHttpRequest()};
+   
+   
     // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readystate === XMLHttpRequest.DONE) { 
+         //take some action
+         if(request.status === 200) {
+             var counter = request.responseText;
+             var span = document.getElementById('count');
+             span.innerHTML = counter.toString();
+
+         }  
+         }
+
+
+    //not done yet
+    };
     
-    // Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-};
+    
+    // make the request
+    request.open('GET', 'http://muhamedshibili.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+   
+   
