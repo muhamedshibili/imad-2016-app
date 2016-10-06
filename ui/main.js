aa0393d48b90188c1ba7c22d81325c7ae77f4172
muhamedshibili <shibili.k.007@gmail.com>
2016-10-06 10:57:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,8 +30,6 @@ button.onclick = function () {
 
 };   
 // Submit name
-var nameInput= document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick  = function () {
 // Create a request object
@@ -63,6 +61,8 @@ submit.onclick  = function () {
     
     
     // Make the request
+    var nameInput= document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://muhamedshibili.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
    
