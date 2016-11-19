4c3db4b6eac0bebe6cc1dc31bcafb6036bf987f6
muhamedshibili <shibili.k.007@gmail.com>
2016-11-19 18:50:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+  host: 'db.imad.hasura-app.io',
+  user: 'muhamedshibili',
+  password: process.env.DB_PASSWORD,
+  port: '5432',
+  database: 'muhameshibili'
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -82,6 +91,20 @@ function createtemplate (data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+}); 
+
+var pool = new Pool(config);
+app.get('/test-db' , function (req, res) {
+   // make a select request
+   // return a response with results
+  pool.query ('SELECT * FROM test' , function(err , result ) {
+  if (err) {
+      res.status(500).send(err.toString());
+  } else {
+      res.send(JSON.stringify(result));
+  }
+  });
+  
 });
 
 var counter = 0;
