f049672c1bf4896c2f5f23ac79954c9e7c809dbc
muhamedshibili <shibili.k.007@gmail.com>
2016-11-20 12:41:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+
 var config = {
     user: 'muhamedshibili',
     database: 'muhamedshibili',
