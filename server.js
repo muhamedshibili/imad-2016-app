039abf6625a2fa4dd3d836864db0bdb1c7bfc27c
muhamedshibili <shibili.k.007@gmail.com>
2016-11-20 11:06:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,42 +15,6 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articles = {
-    'article-one': {
-      title: 'Article One | Tanmai Gopal',
-      heading: 'Article One',
-      date: 'nov 1, 2016',
-      content: `
-          <p>
-              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-          </p>
-          <p>
-              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-          </p>
-          <p>
-              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-          </p>`
-    },
-    'article-two': {
-      title: 'Article Two | Tanmai Gopal',
-      heading: 'Article Two',
-      date: 'Sep 10, 2016',
-      content: `
-          <p>
-              This is the content for my second article.
-          </p>`
-    },
-    'article-three': {
-      title: 'Article Three | muhamed shibili ',
-      heading: 'Article Three',
-      date: 'Sep 15, 2016',
-      content: `
-          <p>
-              This is the content for my third article.
-          </p>`
-    }
-};
-
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
